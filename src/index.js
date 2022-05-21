b5d13c58c72e18f59f482b2c659777fb7d3e60ff
Timothy Barry <44149934+tbarry16@users.noreply.github.com>
2022-05-21 12:33:49 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import TextInput from './lib/components/TextInput.svelte';
-import Accordion from './lib/components/Accordion/Accordion.svelte';
-import Button from './lib/components/Button.svelte';
-import Checkbox from './lib/components/Checkbox.svelte';
-
-export default Svve11 = { TextInput, Accordion, Button, Checkbox };
