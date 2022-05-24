cbf23122ecea96a9ca947fb91fc0f4f3040d1597
tbarry16 <tim.barry12@gmail.com>
2022-05-23 21:16:53 -0400

    meter is pretty much done

---
+++ src/lib/Meter.svelte
@@ -0,0 +1,74 @@
+<script lang='ts'>
+    export let value : number;
+    export let maxValue : number;
+    export let minValue : number;
+
+    export let lowValue : number;
+    export let highValue : number;
+    export let optimumValue : number;
+
+    export let meterLabel : string;
+    export let id : number;
+
+    export let displayPercent : boolean = true;
+    export let displayDecimal : boolean = false;
+    export let units : string = '';
+
+    export let meterStyle : string = ''
+    export let labelStyle : string = ''
+
+    let displayValue : number;
+    let displayString : string;
+
+    if (displayPercent) {
+        displayValue = (value/maxValue) * 100
+        displayString = displayValue + '%'
+    } else if (displayDecimal) {
+        displayValue = value;
+        displayString = displayValue.toString() + units
+    }
+
+    if (minValue > maxValue) {
+        console.log('The min value must be less than the max value')
+    }
+</script>
+
+<label 
+    for={`meter-${id}`} 
+    id={`meter-label-${id}`}
+
+    style={labelStyle}
+>
+    {meterLabel}: {displayString} 
+</label>
+<meter
+    class='meter'
+    id={`meter-${id}`}
+
+    min={minValue}
+    max={maxValue}
+
+    low={lowValue}
+    high={highValue}
+    optimum={optimumValue}
+
+    {value}
+    style={meterStyle}
+
+    aria-valuenow={value} 
+    aria-valuemax={maxValue} 
+    aria-valuemin={minValue}
+    aria-labelledby={`meter-label-${id}`}
+>
+</meter>
+
+<style>
+</style>
+
+
+<!-- The element serving as the meter has a role of meter.
+The meter has aria-valuenow set to a decimal value between aria-valuemin and aria-valuemax representing the current value of the meter.
+The meter has aria-valuemin set to a decimal value less than aria-valuemax.
+The meter has aria-valuemax set to a decimal value greater than aria-valuemin.
+Assistive technologies often present aria-valuenow as a percentage. If conveying the value of the meter only in terms of a percentage would not be user friendly, the aria-valuetext property is set to a string that makes the meter value understandable. For example, a battery meter value might be conveyed as aria-valuetext="50% (6 hours) remaining".
+If the meter has a visible label, it is referenced by aria-labelledby on the element with role meter. Otherwise, the element with role meter has a label provided by aria-label. -->
---
+++ src/routes/pages/meter.svelte
@@ -0,0 +1,31 @@
+<script>
+
+    import Meter from "$lib/Meter.svelte";
+
+    let currValue = 60
+</script>
+
+<div class='test-div'>
+    <Meter 
+        value={currValue}
+        maxValue={100}
+        minValue={0}
+        
+        highValue={80}
+        lowValue={20}
+        optimumValue={80}
+    
+        meterLabel="Demo Meter"
+        id={1}
+
+        labelStyle='font-size: 20px; position: relative; bottom: 30%;'
+        meterStyle='width: 50%; height: 100%;'
+    />
+</div>
+
+<style>
+    .test-div {
+        width: 500px;
+        height: 50px;
+    }
+</style>
