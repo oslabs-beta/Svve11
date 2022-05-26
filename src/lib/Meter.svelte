d88b0696a8d76a213d9775f32ecfee85bad67089
tbarry16 <tim.barry12@gmail.com>
2022-05-26 15:58:56 -0400

    Meter testing done

--- src/lib/Meter.svelte
+++ src/lib/Meter.svelte
@@ -7,7 +7,6 @@
     export let id : number;
 
     //Optional Props
-
     export let lowValue : number;
     export let highValue : number;
     export let optimumValue : number;
@@ -17,9 +16,11 @@
     export let meterStyle : string = ''
     export let labelStyle : string = ''
 
+    //Variables for displaying non percentage label of meter
     let displayValue : number;
     let displayString : string;
-        
+
+    // Build Display String. Default to percentage form unless displayDecimal is provided as true
    if (displayDecimal) {
         displayValue = value;
         displayString = displayValue.toString() + units
@@ -36,7 +37,6 @@
 <label 
     for={`meter-${id}`} 
     id={`meter-label-${id}`}
-
     style={labelStyle}
 >
     {meterLabel}: {displayString} 
@@ -44,32 +44,21 @@
 <meter
     class='meter'
     id={`meter-${id}`}
-
     min={minValue}
     max={maxValue}
-
     low={lowValue}
     high={highValue}
     optimum={optimumValue}
-
     {value}
     style={meterStyle}
-
     aria-valuenow={value} 
     aria-valuemax={maxValue} 
     aria-valuemin={minValue}
     aria-labelledby={`meter-label-${id}`}
     aria-valuetext={valueText}
+    data-testid="meter-test"
 >
 </meter>
 
 <style>
 </style>
-
-
-<!-- The element serving as the meter has a role of meter.
-The meter has aria-valuenow set to a decimal value between aria-valuemin and aria-valuemax representing the current value of the meter.
-The meter has aria-valuemin set to a decimal value less than aria-valuemax.
-The meter has aria-valuemax set to a decimal value greater than aria-valuemin.
-Assistive technologies often present aria-valuenow as a percentage. If conveying the value of the meter only in terms of a percentage would not be user friendly, the aria-valuetext property is set to a string that makes the meter value understandable. For example, a battery meter value might be conveyed as aria-valuetext="50% (6 hours) remaining".
-If the meter has a visible label, it is referenced by aria-labelledby on the element with role meter. Otherwise, the element with role meter has a label provided by aria-label. -->
