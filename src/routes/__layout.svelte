5ddbe4ca6d42ae77b33461999752c5880fbd7ecf
tbarry16 <tim.barry12@gmail.com>
2022-05-27 17:53:24 -0400

    layout and app added

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    import '../app.css'
+    import Navbar from './components/Navbar.svelte'
+</script>
+
+<section class='main-container'>
+    <section id="nav-container">
+        <Navbar />
+      </section>
+
+      <slot></slot>
+</section>
+
+<style>
+
+</style>
