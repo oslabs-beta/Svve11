7546a878b2ba7fd946c40d2dda1e46e44c34b9d9
Simon <simonlee1125@gmail.com>
2022-05-27 17:16:20 -0400

    Fixed an issue where styles weren't being applied to the Table component correctly, which resulted in errors.

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,76 +1,55 @@
 <!-- ************************* SCRIPTS ************************* -->
 <script lang="ts">
-	import type { TableProps } from "./Table/TableTypes";
+	import type { TableProps } from './Table/TableTypes';
 
 	export let tableProps: TableProps = {
 		ariaLabel: '',
 		ariaDescription: '',
 		columnNames: [''],
-		rowsContent: [['']],
-		
-		styles: [
-			// [0] Overall Table styles
-			'',
-			// [1] ColumnName Row styles
-			'',
-			// [2 - ...] Individual Row styles
-			'',
-			// Individual Cell styles?
-		],
-	}
+		rowsContent: [['']]
+	};
 
 	const { id, ariaLabel, ariaDescription } = tableProps;
 	let { columnNames, rowsContent } = tableProps;
-	$: columnNames
-	$: rowsContent
+	$: columnNames;
+	$: rowsContent;
 
-	let overallTableStyles:(string | null) = null
-	let tableTitleStyles:(string | null) = null
-	let headerRowStyles:(string | null) = null
-	// let individualRowStyles:(string[] | null) = null
-
-	$: overallTableStyles
-	$: tableTitleStyles
-	$: headerRowStyles
-	// $: individualRowStyles
-
+	let styles, overallTableStyles, tableTitleStyles, headerRowStyles, genRowStyles;
+	if (tableProps.styles) {
+		styles = tableProps.styles;
+		if (styles.overallTableStyles) overallTableStyles = styles.overallTableStyles;
+		if (styles.tableTitleStyles) tableTitleStyles = styles.tableTitleStyles;
+		if (styles.headerRowStyles) headerRowStyles = styles.headerRowStyles;
+		if (styles.genRowStyles) genRowStyles = styles.genRowStyles;
+	}
 </script>
 
 <!-- ************************* HTML ************************* -->
-<table id={id} aria-label={ariaLabel} aria-describedby={ariaLabel + '_table_desc'}
-	{#if (overallTableStyles !== null)} style={overallTableStyles} {/if}
+<table
+	{id}
+	aria-label={ariaLabel}
+	aria-describedby={ariaLabel + '_table_desc'}
+	style={overallTableStyles ? overallTableStyles : ''}
 >
-	<div id={ariaLabel + '_table_desc'} 
-		{#if tableTitleStyles}
-			style={tableTitleStyles}
-		{:else}
-			style="font-weight: bold; font-size: 125%"
-		{/if}
+	<div
+		id={ariaLabel + '_table_desc'}
+		class="table-title"
+		style={tableTitleStyles ? tableTitleStyles : ''}
 	>
 		{ariaDescription}
 	</div>
 
 	<!-- first row contains Column Names -->
-	<tr id="column-names"
-		{#if headerRowStyles}
-			style={headerRowStyles}
-		{/if}
-	>
-			<!-- populate the columns with each element in the column names array -->
-			{#each columnNames as columnName}
-				<th role="columnheader">{columnName}</th>
-			{/each}
+	<tr id="column-names" style={headerRowStyles ? headerRowStyles : ''}>
+		<!-- populate the columns with each element in the column names array -->
+		{#each columnNames as columnName}
+			<th role="columnheader">{columnName}</th>
+		{/each}
 	</tr>
 
 	<!-- populate table with all row content -->
-	<!-- for each row... -->
-	<!-- {#if individualRowStyles}
-		{#each individualRowStyles as rowStyle, rowIndex}
-			
-		{/each}
-	{/if} -->
 	{#each rowsContent as rowContent}
-		<tr>
+		<tr style={genRowStyles ? genRowStyles : ''}>
 			<!-- for each item in the row... -->
 			{#each rowContent as cellContent}
 				<!-- fill in cell with string -->
@@ -82,16 +61,17 @@
 
 <!-- ************************* STYLES ************************* -->
 <style>
-
 	#column-names {
 		background-color: powderblue;
 	}
 
-	th {
-		font-weight: 500;
+	.table-title {
+		font-weight: bold;
+		font-size: 125%;
+		background-color: none;
 	}
 
-	td {
-	background-color: lightgrey;
+	th {
+		font-weight: 500;
 	}
 </style>
