6668c21d18d8a8cb2e85a9cc62021815374ce9bf
tbarry16 <tim.barry12@gmail.com>
2022-05-27 17:54:15 -0400

    index update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
   const tableProps = {
     id: 'props-table-accordion',
     ariaLabel:'demo',
@@ -12,20 +11,6 @@
       ['Nurbek Jourbourev', '19', 'New Joisey']
     ]
   }
-=======
-	import Navbar from './components/Navbar.svelte';
-	import AccordionPage from './pages/accordion.svelte';
-	import Table from '../lib/Table.svelte';
-	import Button from '$lib/Button.svelte';
-
-	// let pageState = {
-	//   main: true,
-	//   accordion: false,
-	//   button: false,
-	//   checkbox: false,
-	//   textInput: false
-	// }
->>>>>>> main
 
 	const tableProps = {
 		id: 'demo-table',
@@ -47,29 +32,9 @@
 	};
 </script>
 
-<<<<<<< HEAD
   <article class="page-component">
     <header>
     <h1>Welcome to Svve11</h1>
-=======
-<section class="main-container">
-	<section id="nav-container">
-		<Navbar pages="" />
-	</section>
-
-	<article class="page-component">
-		<header>
-			<h1>Welcome to Svve11</h1>
-
-			<p>
-				Here at Svve11, we believe that accessible web development is the future. We know that
-				creating and designing accessible web pages can be a challenge, thus we have created a
-				library of Svelte components built with an accessibility first design strategy. These
-				components can be easily imported into you Svelte files and quickly rendered to your web
-				pages.
-			</p>
-		</header>
->>>>>>> main
 
 		<section>
 			<fieldset>
