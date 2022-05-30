52aae81d6c8e891ab47fb1495610a8f9d1321265
Nurbek <21njouraboev@gmail.com>
2022-05-30 11:40:59 -0400

    radio button

--- src/lib/Checkbox.svelte
+++ src/lib/Checkbox.svelte
@@ -1,27 +1,38 @@
 <script lang="ts">
+	// Label that corresponds to the checkbox
 	export let checkBoxLabel: any;
+
+	// ID of the checkbox
 	export let id: any;
+
+	//Wether the checkbox is checked or not. (By default, the checkbox is not checked)
 	export let checked: boolean = false;
+
+	// Default styling for the label
+	// display: inline-block // Displays the label beside the checkbox
+	// user-select: none; Prevents text selection
 	export let defaultStyle: string = 'display: inline-block; user-select: none;';
+
+	// Styling for the checkbox
 	export let checkBoxStyle: string = '';
+	// Styling for the text
 	export let checkBoxLabelStyle: string = '';
+
+	export let name: string = '';
+	export let value: string = '';
 </script>
 
-<!-- <input aria-label={checkBoxLabel} id={id} type="checkbox" bind:checked />  -->
-<!-- <label for={id}>{checkBoxLabel}</label>  -->
-
-<!-- <input  type="checkbox" aria-label={checkBoxLabel} id={id} name={name} bind:checked>
-<label for={id}>{checkBoxLabel}</label> -->
-<!-- bind:checked={checked} -->
-
+<!-- Rendering the checkbox -->
 <input
 	style={checkBoxStyle}
 	type="checkbox"
 	{id}
 	aria-label={checkBoxLabel}
 	bind:checked
-	name="accessible-checkbox"
+	name=""
+	value=""
 />
+<!-- Rendering the text beside the checkbox -->
 <label style={checkBoxLabelStyle + defaultStyle} for={id}> {checkBoxLabel} </label><br />
 
 <style>
