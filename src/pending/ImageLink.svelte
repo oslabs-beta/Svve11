5ba9855316a503ab5fb65f07ea3ee47ae8ecd2f9
Simon <simonlee1125@gmail.com>
2022-05-31 13:46:39 -0400

    Moved ImageLink component to src/pending. Also refactored the ImageLink component to consolidate props into a single options prop that is an object which takes in props as key/value pairs. imgId and imgClass are optional props now.

---
+++ src/pending/ImageLink.svelte
@@ -0,0 +1,42 @@
+<!-- ************************* SCRIPTS ************************* -->
+<script lang="ts">
+  // define options object type
+  type ImageLinkOptions = {
+    // all attributes are required
+    link: string;
+    src: string;
+    title: string;
+    alt: string;
+    imgId?: string;
+    imgClass?: string;
+  }
+
+  // ImageLink component has five required props, which should all be included in the options prop
+  export let options:ImageLinkOptions = {
+    link: '',
+    src: '',
+    title: '',
+    alt: '',
+  }
+
+  const { link, src, title, alt } = options;
+  let imgId:string, imgClass:string;
+
+  if (options.imgId) imgId = options.imgId;
+  if (options.imgClass) imgClass = options.imgClass;
+
+</script>
+
+
+<!-- ************************* HTML ************************* -->
+<a href={link} class={imgClass ? imgClass + ' sv-imagelink-link' : 'sv-imagelink-link'}>
+  <img id={imgId ? imgId : ''} {src} {title} {alt} target="_blank" >
+  <!-- target="_blank" -> links will always open in a new tab rather than reloading current page -->
+</a>
+
+<!-- ************************* STYLES ************************* -->  
+<style>
+	/*
+    Style these however we'd like later
+  */
+</style>
